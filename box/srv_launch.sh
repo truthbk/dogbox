19fff0acf4effa08e8e69fd400d28cda26102967
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2016-03-17 21:48:00 -0400

    [dogbox] dogbox subscribes to itself, that's where others are publishing.

--- box/srv_launch.sh
+++ box/srv_launch.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-: "${SERVER:?Need to set SERVER env var.}"
 : "${TOPIC:?Need to set TOPIC env var.}"
 
 
 mosquitto -d -c /etc/mosquitto/mosquitto.conf
 sleep 5
-mosquitto_sub -h $SERVER -t $TOPIC
+mosquitto_sub -h 127.0.0.1 -t $TOPIC
 
