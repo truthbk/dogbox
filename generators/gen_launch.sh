cae8451e364bdc9354daa1a093502dad53827068
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2016-03-18 11:20:51 -0400

    [mosquitoes] bring in the generator script in the entrpoint script.

--- generators/gen_launch.sh
+++ generators/gen_launch.sh
@@ -4,10 +4,14 @@
 : "${SERVER:?Need to set SERVER env var.}"
 : "${DEVICE:?Need to set DEVICE env var.}"
 
+curl -o /tmp/generate.py https://raw.githubusercontent.com/deterralba/mqtt-pusher/master/generate.py
+
 if [ ! -e /tmp/generate.py ]; then
     echo "cannot find generator script. Check yourself before you break youself."
     exit 1
 fi
+
+chmod +x /tmp/generate.py
 
 mosquitto -d -c /etc/mosquitto/mosquitto.conf
 sleep 5
