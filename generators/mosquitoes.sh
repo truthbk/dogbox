e58906ed4bdb121caee6c422e60826fc8e614b7e
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2016-03-18 00:11:20 -0400

    [generators] fix the launcher scripts, etc.

---
+++ generators/mosquitoes.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+flavors=( "pressure" "light" "temperature" "humidity" )
+
+if [ $# -ne 3 ];
+then
+    echo "usage: $0 server topic n_instances"
+    exit 1
+fi
+
+for flavor in ${flavors[@]}; do
+    for i in $(seq 1 $3); do
+        docker run -e SERVER="$1" -e TOPIC="$2" -e DEVICE="$flavor" -d mosquitoes
+    done
+done
+
